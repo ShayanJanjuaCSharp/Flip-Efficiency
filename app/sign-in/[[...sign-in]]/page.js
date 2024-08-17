7962daace95f5f776125f0ad4dbb9f8027b6c7c9
Damy Matuwidi <dmatuwidi@gmail.com>
2024-08-17 01:04:22 +0100

    Fixed routing, Changed subscription currency, Fixed AI not working

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,25 @@
+import { SignIn } from "@clerk/nextjs";
+import { AppBar, Box, Button, Container, Link, Toolbar, Typography } from "@mui/material";
+
+let appTitle = "FireFlash"
+
+export default function SignInPage() {
+  return <Container maxWidth="sm">
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{flexGrow: 1}}>{appTitle}</Typography>
+        <Button color="inherit">
+          <Link href="/sign-in" passHref>Sign In</Link>
+        </Button>
+        <Button color="inherit">
+          <Link href="/sign-up" passHref>Sign Up</Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
+
+    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center">
+      <Typography variant="h4">Sign In</Typography>
+      <SignIn />
+    </Box>
+  </Container>
+}
